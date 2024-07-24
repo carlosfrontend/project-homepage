ec2264b637348b5e3c88d5b7bf7e057d91dd454f
carlosfrontend <11649745+carlosfrontend@users.noreply.github.com>
2024-07-24 23:00:00 +0200

    Feat: Working in the projects desktop section

--- src/index.js
+++ src/index.js
@@ -2,13 +2,28 @@ import './css/style.css';
 import devPicture from './images/BW_carlosfrontend_profile.png';
 import ghLogo from './images/github-logo.svg';
 import linkedinLogo from './images/linkedin-logo.svg';
-import twitterlogo from './images/twitter-logo.svg';
+import twitterLogo from './images/twitter-logo.svg';
+import newTabLogo from './images/open-in-new.svg';
 
 const socialMediaContainer = document.querySelectorAll('.social-media-container>a>img');
 
 const devImg = document.querySelector('.header-img');
+const ghImages = [...document.querySelectorAll('.gh-image')];
+const newTabImages = [...document.querySelectorAll('.new-tab-image')];
 devImg.src = devPicture;
 
 [...socialMediaContainer][0].src = ghLogo;
 [...socialMediaContainer][1].src = linkedinLogo;
-[...socialMediaContainer][2].src = twitterlogo;
+[...socialMediaContainer][2].src = twitterLogo;
+
+newTabImages.map(el => {
+  const icon = el;
+  icon.src = newTabLogo;
+  return true;
+});
+
+ghImages.map(el => {
+  const icon = el;
+  icon.src = ghLogo;
+  return true;
+});
