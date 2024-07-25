96a8d8c2f1c6e13b9dfbe9b10141998ad9bd2001
carlosfrontend <11649745+carlosfrontend@users.noreply.github.com>
2024-07-25 14:51:55 +0200

    Feat: Working in Contact desktop section

--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ import ghLogo from './images/github-logo.svg';
 import linkedinLogo from './images/linkedin-logo.svg';
 import twitterLogo from './images/twitter-logo.svg';
 import newTabLogo from './images/open-in-new.svg';
+import workingPicture from './images/working.png';
 
 const socialMediaContainer = document.querySelectorAll('.social-media-container>a>img');
 
 const devImg = document.querySelector('.header-img');
 const ghImages = [...document.querySelectorAll('.gh-image')];
 const newTabImages = [...document.querySelectorAll('.new-tab-image')];
+const workingImg = document.querySelector('.img-container>img');
+const footerGHLogo = document.querySelector('.footer-social-media > a > img');
+const footerLinkedinLogo = document.querySelector('.footer-social-media > a:nth-child(2) > img');
+const footerTwitterLogo = document.querySelector('.footer-social-media > a:nth-child(3) > img');
 devImg.src = devPicture;
 
 [...socialMediaContainer][0].src = ghLogo;
@@ -27,3 +32,9 @@ ghImages.map(el => {
   icon.src = ghLogo;
   return true;
 });
+
+workingImg.src = workingPicture;
+
+footerGHLogo.src = ghLogo;
+footerLinkedinLogo.src = linkedinLogo;
+footerTwitterLogo.src = twitterLogo;
